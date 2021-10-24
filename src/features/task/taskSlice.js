01d7384136f8cc4bed42e2aa817043e5e3b0e8fc
Toshinori Sumida <t.sumida1987@gmial.com>
2021-10-25 01:55:45 +0900

    TaskSlice

---
+++ src/features/task/taskSlice.js
@@ -0,0 +1,57 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  idCount: 3,
+  tasks: [
+    {
+      id: 1,
+      title: 'TASK A',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'TASK B',
+      completed: true,
+    },
+    {
+      id: 3,
+      title: 'TASK C',
+      completed: false,
+    },
+  ],
+}
+
+export const taskSlice = createSlice({
+  name: 'task',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    newTask: (state, action) => {
+      state.idCount++
+      const newItem = {
+        id: state.idCount,
+        title: action.payload,
+        completed: false,
+      }
+      state.tasks = [newItem, ...state.tasks]
+    },
+    completeTask: (state, action) => {
+      const task = state.tasks.find((t) => t.id === action.payload.id)
+      if (task) {
+        task.completed = !task.completed
+      }
+    },
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    deleteTask: (state, action) => {
+      state.tasks = state.tasks.filter((t) => t.id !== action.payload.id)
+    },
+  },
+})
+
+export const { newTask, completeTask, deleteTask } = taskSlice.actions
+
+export const selectCount = (state) => state.counter.value
+
+export const selectTasks = (state) => state.task.tasks
+
+export default taskSlice.reducer
